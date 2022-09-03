a5b33262d36e4f61d14e8b5d036468b805cf872c
Michael <michaelbrooks6897@gmail.com>
2022-09-02 23:46:23 -0400

    nevermind

--- src/Components/GameContentParts/Game.tsx
+++ src/Components/GameContentParts/Game.tsx
@@ -5,13 +5,6 @@ function Game(props:any){
 
     let elements = "";
 
-    for(let i = 0; i < 6; i++){
-        let innerElements = "";
-        for(let j = 0; i < props.count; j++){
-           innerElements += "<LetterSpace row={" + i + "} collumn={" + j + "}/>"; 
-        }
-        elements+=innerElements;
-    }
 
     let total = <LetterWrapper elements={elements}/>;
     
